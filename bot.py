4a38168f5ceade3457d3869e2faaced12cdb16a5
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-02-20 08:56:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from datetime import datetime, timedelta
 from typing import List, Dict, Optional, Tuple, Union
 
 from pyrogram import Client, filters, enums
+from pyrogram.handlers import MessageHandler
 from pyrogram.types import (
     Message,
     InlineKeyboardMarkup,
