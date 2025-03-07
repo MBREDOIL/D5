a6f4d18c5ba0c35e77e8acf23e4da71867e00b45
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-03-07 22:49:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ import mimetypes
 import pytz
 import fitz  # PyMuPDF
 import shutil
+import tempfile
 from pathlib import Path
 from datetime import datetime
 from urllib.parse import urlparse, urljoin, unquote, quote, urlunparse
